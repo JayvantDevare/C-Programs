9aae201f81996db1a270c4349e3fd61af9f253de
Jayvant Devare <jayvant.devare@gmail.com>
2021-01-12 15:59:17 +0530

    C Program to display "Welcome All" on screen

---
+++ FirstProgram.c
@@ -0,0 +1,15 @@
+//Program to display Welcome All on screen
+
+//Header Files section
+
+#include <stdio.h>   //stdio.h standard input output header file
+
+
+//main function section
+int main()
+{   
+    printf("Welcome All");  //printf() function used to print message on screen
+
+    return 0;
+}
+//end of main function
